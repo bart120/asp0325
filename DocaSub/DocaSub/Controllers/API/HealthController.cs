cef8318858d317c8a88fb7f79465611da0f59fab
Vincent Leclerc <vleclerc@inow.fr>
2025-03-31 14:48:56 +0200

    layout

---
+++ DocaSub/DocaSub/Controllers/API/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocaSub.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new {status = "Heathly"});
+        }
+    }
+}
--- DocaSub/DocaSub/Program.cs
+++ DocaSub/DocaSub/Program.cs
@@ -1,18 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
-
+//services
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();// razor pages
+
+builder.Services.AddControllersWithViews();/*.AddRazorOptions(options =>
+{
+    options.ViewLocationFormats.Clear();
+    options.ViewLocationFormats.Add("MesVues/{1}/{0}.cshtml");
+});*/ // mvc
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//déclaration midelwares
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -20,6 +26,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+//app.MapControllers();
+app.MapControllerRoute( // mvc
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.MapRazorPages(); // razor pages
 
 app.Run();
