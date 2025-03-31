695f8be88a14bbef9b2d6d2507f036a47e8f9946
Vincent Leclerc <vleclerc@inow.fr>
2025-03-31 17:11:45 +0200

    viewmodels

--- DocaSub/DocaSub/Controllers/SubRequestsController.cs
+++ DocaSub/DocaSub/Controllers/SubRequestsController.cs
@@ -1,4 +1,5 @@
 ﻿using DocaSub.Models;
+using DocaSub.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DocaSub.Controllers
@@ -14,7 +15,20 @@ namespace DocaSub.Controllers
                 new SubRequest { Id = 2, Title = "Demande 2", Amount = 2000, Status = 2, CreatedAt=DateTime.Now },
                 new SubRequest { Id = 3, Title = "Demande 3", Amount = 3000, Status = 3, CreatedAt=DateTime.Now }
             };
-            return View(liste);
+
+            var liste2 = new List<SubRequest>
+            {
+                new SubRequest { Id = 1, Title = "Demande 5", Amount = 1000, Status = 1, CreatedAt=DateTime.Now },
+                new SubRequest { Id = 2, Title = "Demande 6", Amount = 2000, Status = 2, CreatedAt=DateTime.Now },
+                new SubRequest { Id = 3, Title = "Demande 7", Amount = 3000, Status = 3, CreatedAt=DateTime.Now }
+            };
+
+            var model = new SubRequestIndexViewModel
+            {
+                SubRequestActive = liste,
+                SubRequesteOlder = liste2
+            };
+            return View(model);
         }
     }
 }
---
+++ DocaSub/DocaSub/Models/ViewModels/SubRequestViewModels.cs
@@ -0,0 +1,14 @@
+﻿namespace DocaSub.Models.ViewModels
+{
+    public class SubRequestIndexViewModel
+    {
+        public IEnumerable<SubRequest> SubRequestActive { get; set; }
+
+        public IEnumerable<SubRequest> SubRequesteOlder { get; set; }
+    }
+
+
+    public class SubRequestCreateViewModel
+    {
+    }
+}
