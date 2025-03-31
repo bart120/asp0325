d26998e821261badd501e72aec3371bc0ca2266c
Vincent Leclerc <vleclerc@inow.fr>
2025-03-31 16:59:11 +0200

    models

---
+++ DocaSub/DocaSub/Controllers/SubRequestsController.cs
@@ -0,0 +1,20 @@
+﻿using DocaSub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocaSub.Controllers
+{
+    public class SubRequestsController : Controller
+    {
+        [Route("mes-demandes")]
+        public IActionResult Index()
+        {
+            var liste = new List<SubRequest>
+            {
+                new SubRequest { Id = 1, Title = "Demande 1", Amount = 1000, Status = 1, CreatedAt=DateTime.Now },
+                new SubRequest { Id = 2, Title = "Demande 2", Amount = 2000, Status = 2, CreatedAt=DateTime.Now },
+                new SubRequest { Id = 3, Title = "Demande 3", Amount = 3000, Status = 3, CreatedAt=DateTime.Now }
+            };
+            return View(liste);
+        }
+    }
+}
---
+++ DocaSub/DocaSub/Models/SubRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace DocaSub.Models
+{
+    public class SubRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public decimal Amount { get; set; } = 0;
+        public int Status { get; set; } = 0;
+        public DateTime CreatedAt { get; set; }
+    }
+}
