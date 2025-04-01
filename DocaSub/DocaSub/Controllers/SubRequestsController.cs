c82326a6aeb2ae64b0de6bacf3e584cfaac0106a
Vincent Leclerc <vleclerc@inow.fr>
2025-04-01 15:44:54 +0200

    TempData

--- DocaSub/DocaSub/Controllers/SubRequestsController.cs
+++ DocaSub/DocaSub/Controllers/SubRequestsController.cs
@@ -25,19 +25,21 @@ namespace DocaSub.Controllers
                 new SubRequest { Id = 2, Title = "Demande 2", Amount = 2000, Status = 2, CreatedAt=DateTime.Now },
                 new SubRequest { Id = 3, Title = "Demande 3", Amount = 3000, Status = 3, CreatedAt=DateTime.Now }
             };*/
-            var liste =  _dbContext.SubRequests.ToList();
+            /*var liste =  _dbContext.SubRequests.ToList();
 
             var liste2 = new List<SubRequest>
             {
                 new SubRequest { Id = 1, Title = "Demande 5", Amount = 1000, Status = 1, CreatedAt=DateTime.Now },
                 new SubRequest { Id = 2, Title = "Demande 6", Amount = 2000, Status = 2, CreatedAt=DateTime.Now },
                 new SubRequest { Id = 3, Title = "Demande 7", Amount = 3000, Status = 3, CreatedAt=DateTime.Now }
-            };
+            };*/
 
             var model = new SubRequestIndexViewModel
             {
-                SubRequestActive = liste,
-                SubRequesteOlder = liste2
+                SubRequestActive = _dbContext.SubRequests.Where(x => x.Status != 0),
+                SubRequesteOlder = from x in _dbContext.SubRequests
+                                   where x.Status == 0
+                                   select x
             };
             return View(model);
         }
@@ -51,11 +53,19 @@ namespace DocaSub.Controllers
 
         [HttpPost]
         //[ActionName("Create")]
-        public IActionResult Create([FromForm]SubRequest subRequest)
+        public async Task<IActionResult> Create([FromForm]SubRequest subRequest)
         {
             if (ModelState.IsValid)
             {
-
+                subRequest.CreatedAt = DateTime.Now;
+                subRequest.Priority = 1;
+                subRequest.Status = 1;
+                _dbContext.SubRequests.Add(subRequest);
+                await _dbContext.SaveChangesAsync();
+                //ViewBag.SuccessMessage = "Demande enregistrée avec succès";
+                TempData["SuccessMessage"] = "Demande enregistrée avec succès";
+                //return View(subRequest);
+                return RedirectToAction("Index");
             }
             return View(subRequest);
         }
